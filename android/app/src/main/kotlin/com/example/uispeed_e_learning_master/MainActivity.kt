ea6a69b3785103a2beef8d61a5488afcee8eec06
gilang nuril <gilangn89@gmail.com>
2022-03-28 17:54:04 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/uispeed_e_learning_master/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uispeed_e_learning_master
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:uispeed_e_learning_master/page/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
